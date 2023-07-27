f75ee2b702e61832b82d33b29f2c8e58ad6cad99
eucarizan <eucarizan@gmail.com>
2023-07-28 07:44:16 +0800

    sample problems added: composite number, number sign

--- SampleProblems/CompositeNumber/src/main/java/Main.java
+++ SampleProblems/CompositeNumber/src/main/java/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final long number= scanner.nextInt();
+        System.out.println(isComposite(number));
+    }
+
+    private static boolean isComposite(long number) {
+        for (long i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- SampleProblems/NumberSign/src/main/java/Main.java
+++ SampleProblems/NumberSign/src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(sign(n));
+    }
+
+    private static int sign(int number) {
+//        return Integer.compare(number, 0);
+        return number == 0 ? 0 : number / Math.abs(number);
+    }
+
 }
