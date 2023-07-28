66bfbc19b8013b24fe19973e27cb337285e76429
eucarizan <eucarizan@gmail.com>
2023-07-28 17:08:39 +0800

    Java Backend: Rock-Paper-Scissors stage 3 implementation

--- Java Backend/RockPaperScissors/README.md
+++ Java Backend/RockPaperScissors/README.md
@@ -12,6 +12,10 @@
       - [2.1 Description](#21-description)
       - [2.2 Objectives](#22-objectives)
       - [2.3 Example](#23-example)
+    - [3: Endless game](#3-endless-game)
+      - [3.1 Description](#31-description)
+      - [3.2 Objectives](#32-objectives)
+      - [3.3 Example](#33-example)
 
 ## Learning Outcomes
 A playable Rock-Paper-Scissors game, with a Player vs. Computer mode. Practice using arrays, the Random class, formatted strings, and algorithms.
@@ -96,4 +100,42 @@ There is a draw (scissors)
 ```console
 > paper
 Sorry, but the computer chose scissors
+```
+
+### 3: Endless game
+#### 3.1 Description
+We came up with a really cool idea in the previous stage. But the game is really short. Nobody plays a single shot of rock paper scissors. We need to find a way to run the game forever. Not literally, though — let's implement a way to stop your program.
+
+Improve your program so that it will take an unlimited number of inputs until users enter `!exit`. After entering `!exit`, the program should print `Bye!` and terminate. Also, let's try to handle invalid inputs: your program should be ready to handle typos in user input, or when there's a mishmash instead of a normal command. So, if the input doesn't correspond to any known command (an option or `!exit`), your program should output the following line: `Invalid input`.
+
+#### 3.2 Objectives
+Your program should:
+- Take input from users;
+- If the input contains `!exit`, output `Bye!` and stop the game;
+- If the input is the name of the option, then pick a random option and output a line with the result of the game in the following format (`<option>` is the name of the option chosen by the program):
+  - Loss: `Sorry, but the computer chose <option>`
+  - Draw: `There is a draw (<option>)`
+  - Win: `Well done. The computer chose <option> and failed`
+- If the input corresponds to anything else, output Invalid input;
+- Repeat it all over again.
+
+#### 3.3 Example
+The greater-than symbol followed by a space (`> `) represents the user input. Note that it's not part of the input.
+
+**Example 3.3.1:**
+```console
+> rock
+Well done. The computer chose scissors and failed
+> paper
+Well done. The computer chose rock and failed
+> paper
+There is a draw (paper)
+> scissors
+Sorry, but the computer chose rock
+> rokc
+Invalid input
+> xit!
+Invalid input
+> !exit
+Bye!
 ```
--- Java Backend/RockPaperScissors/src/main/java/Main.java
+++ Java Backend/RockPaperScissors/src/main/java/Main.java
@@ -2,32 +2,64 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static final String msgLoss = "Sorry, but the computer chose %s";
+    static final String msgDraw = "There is a draw (%s)";
+    static final String msgWin = "Well done. The computer chose %s and failed";
+    static final String[] hands = {"paper", "scissors", "rock"};
+
     public static void main(String[] args) {
+        play();
+    }
+
+    private static void play() {
         Scanner scanner = new Scanner(System.in);
-        final String[] hands = {"paper", "scissors", "rock"};
+
+        String userHand = scanner.nextLine();
+
+        while (!userHand.equals("!exit")) {
+            switch (userHand) {
+                case "rock", "paper", "scissors" -> getResult(userHand);
+                default -> System.out.println("Invalid input");
+            }
+            userHand = scanner.nextLine();
+        }
+
+        System.out.println("Bye!");
+    }
+
+    private static void getResult(String userHand) {
         StringBuilder response = new StringBuilder();
 
-        String msgLoss = "Sorry, but the computer chose %s";
-        String msgDraw = "There is a draw (%s)";
-        String msgWin = "Well done. The computer chose %s and failed";
-
-        String user = scanner.nextLine();
         Random random = new Random();
         String compHand = hands[random.nextInt(3)];
 
         // computer win
-        boolean paperScissors = user.equals(hands[0]) && compHand.equals(hands[1]);
-        boolean scissorsRock = user.equals(hands[1]) && compHand.equals(hands[2]);
-        boolean rockPaper = user.equals(hands[2]) && compHand.equals(hands[0]);
+        boolean paperScissors = userHand.equals(hands[0]) && compHand.equals(hands[1]);
+        boolean scissorsRock = userHand.equals(hands[1]) && compHand.equals(hands[2]);
+        boolean rockPaper = userHand.equals(hands[2]) && compHand.equals(hands[0]);
 
         if (paperScissors || scissorsRock || rockPaper) {
-            response.append(String.format(msgLoss, compHand));
-        } else if (user.equals(compHand)) {
-            response.append(String.format(msgDraw, compHand));
+            response.append(getResultMsg("loss", compHand));
+        } else if (userHand.equals(compHand)) {
+            response.append(getResultMsg("draw", compHand));
         } else {
-            response.append(String.format(msgWin, compHand));
+            response.append(getResultMsg("win", compHand));
         }
 
         System.out.println(response);
+    }
+
+    private static String getResultMsg(String result, String compHand) {
+        switch (result) {
+            case "loss" -> {
+                return String.format(msgLoss, compHand);
+            }
+            case "draw" -> {
+                return String.format(msgDraw, compHand);
+            }
+            default -> {
+                return String.format(msgWin, compHand);
+            }
+        }
     }
 }
