48a0801472633aa5a65599698736c7fe80d600d9
eucarizan <eucarizan@gmail.com>
2023-08-09 00:52:28 +0800

    Java Backend: Bulls and Cows stage 4 implementation

--- Java Backend/BullsAndCows/README.md
+++ Java Backend/BullsAndCows/README.md
@@ -15,6 +15,10 @@
       - [3.1 Description](#31-description)
       - [3.2 Objectives](#32-objectives)
       - [3.3 Examples](#33-examples)
+    - [4: Game time!](#4-game-time)
+      - [4.1 Description](#41-description)
+      - [4.2 Objectives](#42-objectives)
+      - [4.3 Examples](#43-examples)
 
 ## Learning outcomes
 Learn how to use integer arithmetic, generate random numbers, store data in data structures, and handle errors.
@@ -178,3 +182,37 @@ The random secret number is 5213.
 > 11
 Error: can't generate a secret number with length of 11 because there aren't enough unique digits.
 ```
+
+### 4: Game time!
+#### 4.1 Description
+In this stage, you should combine all the previous parts into a simple playable version of the "Bulls and Cows" game. First, prompt the player to input the length of the secret code. The length will determine the difficulty level for the current game session. The program should generate a secret code of the given length. Remember that it should consist of unique numbers.
+
+Then, the game starts and the program prompts the player to guess the code. When the player inputs a number, the program should grade it in bulls and cows. The game goes on until the code is guessed, that is, until the number of bulls is equal to the number of digits in the code. When the game ends, the program should finish its execution.
+
+#### 4.2 Objectives
+In this stage, your program should:
+
+1. Ask for the length of the secret code and then generate the code.
+2. Wait for the user input.
+3. Grade the guessing attempt in bulls and cows.
+4. If the secret code has been guessed, the program stops; otherwise, return to the second step.
+
+#### 4.3 Examples
+The greater-than symbol followed by a space (`> `) represents the user input. Note that it's not part of the input.
+
+```console
+Please, enter the secret code's length:
+> 4
+Okay, let's start a game!
+Turn 1:
+> 1234
+Grade: 1 bull and 1 cow
+Turn 2:
+> 7354
+Grade: 2 bulls and 1 cow
+Turn 3:
+> 9374
+Grade: 4 bulls
+Congratulations! You guessed the secret code.
+```
+
--- Java Backend/BullsAndCows/src/main/java/Main.java
+++ Java Backend/BullsAndCows/src/main/java/Main.java
@@ -1,23 +1,43 @@
 import java.util.Random;
 import java.util.Scanner;
 
+@SuppressWarnings("unused")
 public class Main {
-    static final String secretNumber = "1234";
+    static String secretNumber;
+    static boolean guessed = false;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-//            String guess = scanner.nextLine();
-//            count(guess);
-            int length = scanner.nextInt();
-            createSecretNumber(length);
-//            while (length != 0) {
-//                createSecretNumber(length);
-//                length = scanner.nextInt();
-//            }
+
+            String tempSecretNumber = "";
+            do {
+                System.out.println("Please, enter the secret code's length");
+                int length = Integer.parseInt(scanner.nextLine());
+                tempSecretNumber = createSecretNumber(length);
+            } while ("recreate".equals(tempSecretNumber));
+
+            secretNumber = tempSecretNumber;
+            play(scanner);
         }
     }
 
-    private static void count(String guess) {
+    private static void play(Scanner scanner) {
+        System.out.println("Okay, let's start a game!");
+        int turn = 1;
+        final int bullCount = secretNumber.length();
+
+        while (!guessed) {
+            System.out.println("Turn " + turn++ + ":");
+            String guess = scanner.nextLine();
+            if (bullCount == count(guess)) {
+                guessed = true;
+            }
+        }
+
+        System.out.println("Congratulations! You guessed the secret code.");
+    }
+
+    private static int count(String guess) {
         int bullCount = 0;
         int cowCount = 0;
         int length = secretNumber.length();
@@ -35,29 +55,34 @@ public class Main {
         }
 
         print(bullCount, cowCount);
+
+        return bullCount;
     }
 
     private static void print(int bullCount, int cowCount) {
         StringBuilder strOut = new StringBuilder("Grade: ");
 
         if (bullCount > 0) {
-            strOut.append(String.format("%d bull(s)", bullCount));
+            strOut.append(String.format("%d bull", bullCount));
+//            strOut.append(String.format("%d bull(s)", bullCount));
         }
         if (bullCount > 0 && cowCount > 0) {
             strOut.append(" and ");
         }
         if (cowCount > 0) {
-            strOut.append(String.format("%d cow(s)", cowCount));
+            strOut.append(String.format("%d cow", cowCount));
+            // strOut.append(String.format("%d cow(s)", cowCount));
         }
 
         if (bullCount == 0 && cowCount == 0) {
             strOut.append("None");
         }
 
-        System.out.println(strOut.append(String.format(". The secret code is %s.", secretNumber)));
+        System.out.println(strOut);
+        // System.out.println(strOut.append(String.format(". The secret code is %s.", secretNumber)));
     }
 
-    private static void createSecretNumber(int length) {
+    private static String createSecretNumber(int length) {
         final long pseudoRandomNumber = System.nanoTime();
 
         final long[] lowerBound = { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
@@ -67,7 +92,8 @@ public class Main {
 
         if (length > 10) {
             System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
-            return;
+            // System.out.println("Error: can't generate a secret number with a length > 10 because there aren't enough unique digits.");
+            return "recreate";
         }
 
         long num = random.nextLong(lowerBound[index], upperBound[index]);
@@ -78,7 +104,7 @@ public class Main {
             digit0 = String.valueOf(num).charAt(0);
         }
 
-        System.out.println("The random secret number is " + num);
+        return String.valueOf(num);
     }
 
     private static boolean isDistinct(long num, int length) {
