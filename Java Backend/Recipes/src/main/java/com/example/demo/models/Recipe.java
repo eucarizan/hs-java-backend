31edb361eac679db301252c38433c1c65eff4702
eucarizan <eucarizan@gmail.com>
2023-11-16 16:20:47 +0800

    Java Backend: Recipes stage 3 implementation - tried json deserialize

--- Java Backend/Recipes/src/main/java/com/example/demo/models/Recipe.java
+++ Java Backend/Recipes/src/main/java/com/example/demo/models/Recipe.java
@@ -1,39 +1,45 @@
 package com.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
 
-@Data
 @Builder
+@Getter
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "recipe")
+@JsonDeserialize(builder = Recipe.RecipeBuilder.class)
+@JsonIncludeProperties({"name", "description", "ingredients", "directions"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({ "id" })
 public class Recipe {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @Column
-    @NonNull
-    String name;
+    @NonNull String name;
 
     @Column
-    @NonNull
-    String description;
+    @NonNull String description;
 
     @Singular
     @Column
-    @NonNull
-    List<String> ingredients;
+    @NonNull List<String> ingredients;
 
     @Singular
     @Column
-    @NonNull
-    List<String> directions;
+    @NonNull List<String> directions;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class RecipeBuilder {
+    }
 }
