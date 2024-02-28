3dfb8ade459bf372a5cbbada50511a92308b8f20
eucarizan <eucarizan@gmail.com>
2024-02-28 18:46:40 +0800

    Java Backend: WebQuiz stage 1 - preparation

---
+++ Java Backend/WebQuiz/src/test/java/dev/nj/webquiz/WebquizApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nj.webquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
